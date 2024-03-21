0e5344f9323b11bafecf742c0f98b308ed63cd96
seanposner <50656530+seanposner@users.noreply.github.com>
2024-03-21 14:27:33 +0200

    TF working

--- infrastructure/localstack.tf
+++ infrastructure/localstack.tf
@@ -12,7 +12,7 @@ provider "docker" {}
 
 # Initiate LocalStack container
 resource "docker_image" "localstack" {
-  name         = "localstack/localstack"
+  name         = "localstack/localstack-pro"
 }
 
 resource "docker_container" "localstack_container" {
@@ -23,8 +23,10 @@ resource "docker_container" "localstack_container" {
     external = 4566
   }
   env = [
-    "SERVICES=s3,sqs,ecs,ecr,iam",
+    "SERVICES=s3,sqs,ecs,ecr,iam,ec2",
     "DEBUG=1",
-    "DATA_DIR=/tmp/localstack/data"
+    "DATA_DIR=/tmp/localstack/data",
+    "ACTIVATE_PRO=1",
+    "LOCALSTACK_AUTH_TOKEN='ls-POteMEvE-qIQI-bimO-9332-5110wUkA2dc4'"
   ]
 }
--- infrastructure/main/main.tf
+++ infrastructure/main/main.tf
@@ -29,50 +29,6 @@ resource "aws_ecr_repository" "sqs" {
   image_tag_mutability = "MUTABLE"
 }
 
-resource "aws_ecs_task_definition" "elb_task" {
-  family                   = "elb-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "elb"
-      image     = "${aws_ecr_repository.elb.repository_url}:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 5000
-        }
-      ]
-    }
-  ])
-}
-
-resource "aws_ecs_task_definition" "sqs_task" {
-  family                   = "sqs-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "sqs"
-      image     = "${aws_ecr_repository.sqs.repository_url}:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 5001
-        }
-      ]
-    }
-  ])
-}
-
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 
@@ -96,13 +52,94 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_service" "elb_service" {
-  name            = "elb-service"
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.elb_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "sg" {
+  name        = "sg"
+  description = "Security group for ECS tasks"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_task_definition" "elb_task" {
+  depends_on = [
+    aws_ecr_repository.elb,
+    aws_iam_role.ecs_task_execution_role
+  ]
+
+  family                   = "elb-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name         = "elb"
+      image        = "${aws_ecr_repository.elb.repository_url}:latest"
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 5000
+          hostPort      = 5000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "sqs_task" {
+  depends_on = [
+    aws_ecr_repository.elb,
+    aws_iam_role.ecs_task_execution_role
+  ]
+
+  family                   = "sqs-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name         = "sqs"
+      image        = "${aws_ecr_repository.elb.repository_url}:latest"
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 5001
+          hostPort      = 5001
+        }
+      ]
+    }
+  ])
+}
+
 
 resource "aws_ecs_service" "sqs_service" {
   name            = "sqs-service"
@@ -110,4 +147,24 @@ resource "aws_ecs_service" "sqs_service" {
   task_definition = aws_ecs_task_definition.sqs_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.example_subnet.id]
+    security_groups = [aws_security_group.sg.id]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_ecs_service" "elb_service" {
+  name            = "elb-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.elb_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.example_subnet.id]
+    security_groups = [aws_security_group.sg.id]
+    assign_public_ip = true
+  }
 }
