ed706d399fdd2f96ff2af918b39d0e86670ef353
seanposner <50656530+seanposner@users.noreply.github.com>
2024-03-21 13:44:21 +0200

    Updates

--- sqs.py
+++ sqs.py
@@ -7,7 +7,7 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# SQS and S3 client initialization (configure for LocalStack)
+# SQS and S3 client initialization
 sqs = boto3.client('sqs', endpoint_url='http://localhost:4566',
                    region_name='us-east-1',
                    aws_access_key_id='test',
@@ -22,13 +22,13 @@ QUEUE_NAME = 'sqs'
 S3_BUCKET_NAME = 'main'
 
 def ensure_resources():
-    # Ensure SQS queue exists
+    # Ensure SQS queue
     try:
         sqs.get_queue_url(QueueName=QUEUE_NAME)
     except sqs.exceptions.QueueDoesNotExist:
         sqs.create_queue(QueueName=QUEUE_NAME)
 
-    # Ensure S3 bucket exists
+    # Ensure S3 bucket
     if S3_BUCKET_NAME not in [bucket['Name'] for bucket in s3.list_buckets().get('Buckets', [])]:
         s3.create_bucket(Bucket=S3_BUCKET_NAME)
 
@@ -43,14 +43,11 @@ def poll_sqs_and_upload_to_s3():
                 message_body = msg['Body']
                 receipt_handle = msg['ReceiptHandle']
                 
-                # Upload to S3
                 s3.put_object(Bucket=S3_BUCKET_NAME, Key=f'uploads/{msg["MessageId"]}.txt', Body=message_body)
                 
-                # Delete the message from the queue after processing
                 sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
                 logger.info(f"Uploaded message {msg['MessageId']} to S3 and deleted from SQS.")
         else:
-            # Sleep if there are no messages to avoid flooding the log
             time.sleep(5)
 
 if __name__ == '__main__':
